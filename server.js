2c73f5fedf64003f09cfbed7479b236a2330a0d4
samjbro <sjbroughton1@gmail.com>
2017-03-03 13:33:15 +0000

    configures ejs

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', function(req,res){
-  res.send('hi bae');
-});
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+
+app.get('/', (req,res) => res.render('index'));
 
 app.listen(3000)
